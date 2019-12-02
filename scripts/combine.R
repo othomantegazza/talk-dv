20d38d0769c0544149485df149f7d848bf860240
Otho <otho.mantegazza@gmail.com>
2019-12-02 12:06:43 +0100

    ch plots

--- scripts/combine.R
+++ scripts/combine.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+theme_set(theme_minimal())
+
 calvin <-
   read_csv(here::here("data/calvin.csv")) %>% 
   mutate(y = y*1.3,
@@ -8,17 +10,39 @@ calvin <-
          y = y+300)
 hobbes <- read_csv(here::here("data/hobbes.csv"))
 
-bind_rows(calvin, hobbes) %>% 
+
+tiger_attack <- 
+  bind_rows(calvin, hobbes)
+
+p <- 
+  tiger_attack %>% 
   ggplot(aes(x = x,
-             y = max(y) - y)) +
-  # geom_smooth(method = "lm") +
-  # geom_smooth() +
-  geom_point(size = .5,
-             alpha = 1) +
-  # geom_rug() +
-  # scale_y_reverse() +
-  coord_fixed() +
+             y = max(y) - y))  +
   lims(x = c(0, 1100),
        y = c(0, NA)) +
-  theme_bw()
+  coord_fixed() 
 
+p1 <- 
+  p +
+  geom_smooth(colour = "#235596", method = "lm")
+
+p2 <- 
+  p +
+  geom_smooth(colour = "#fc8f0033", method = "lm") +
+  geom_smooth(colour = "#235596", method = "lm", formula = y ~ poly(x, 2))
+
+p3 <- 
+  p +
+  geom_smooth(colour = "#fc8f0033", method = "lm") +
+  geom_smooth(colour = "#fc8f0033", method = "lm", formula = y ~ poly(x, 2)) +
+  geom_smooth(colour = "#235596", method = "lm", formula = y ~ poly(x, 5))
+
+p4 <- 
+  p +
+  geom_smooth(colour = "#fc8f0005", method = "lm") +
+  geom_smooth(colour = "#fc8f0005", method = "lm", formula = y ~ poly(x, 2)) + 
+  geom_smooth(colour = "#fc8f0005", method = "lm", formula = y ~ poly(x, 5)) +
+  geom_point(size = .5,
+             alpha = 1) 
+
+p4
